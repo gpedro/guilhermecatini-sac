1f6f99a40e113b1612bd996a1925cbdfe10069a1
Guilherme Catini <guilherme@catini.org>
2016-07-12 09:30:07 -0300

    BD

--- _lib/classes/Conexao.class.php
+++ _lib/classes/Conexao.class.php
@@ -3,7 +3,7 @@ class Conexao {
 
     public static function getCon() {
         try {
-            $db = new PDO("pgsql:host=104.236.88.179 dbname=dbsac user=postgres password=weiss");
+            $db = new PDO("pgsql:host=104.236.88.179 dbname=dev_sac user=dev_sac password=1234");
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             return $db;
